b1dda38eec32c468a34f34f50ac2db95cb8ab131
Florence Mary <missflorencemary@hotmail.com>
2024-12-04 17:11:18 +0100

    Day 1 Fix scope issue / class declaration

--- src/main/scala/dayOne.scala
+++ src/main/scala/dayOne.scala
@@ -1,33 +1,35 @@
 package com.tcsw.aoc
 
-class dayOne
+class dayOne {
 
-val exampleSource = "src/main/resources/1/example_input.txt"
-val actualSource = "src/main/resources/1/actual_input.txt"
+  val exampleSource = "src/main/resources/1/example_input.txt"
+  val actualSource = "src/main/resources/1/actual_input.txt"
 
-def solveDayOne(): Unit =
+  def solveDayOne(): Unit =
 
-  val source = scala.io.Source.fromFile(actualSource)
-  val lines = try source.getLines().toList finally source.close()
-  val (leftList, rightList) = getLeftAndRightLists(lines)
+    val source = scala.io.Source.fromFile(actualSource)
+    val lines = try source.getLines().toList finally source.close()
+    val (leftList, rightList) = getLeftAndRightLists(lines)
 
-  //print(partOne(leftList, rightList))
-  print(partTwo(leftList, rightList))
+    //print(partOne(leftList, rightList))
+    print(partTwo(leftList, rightList))
 
-def partOne(firstList: List[Int], secondList: List[Int]) =
-  firstList.sorted
-    .zip(secondList.sorted)
-    .map((leftNumber, rightNumber) =>  (rightNumber - leftNumber).abs)
-    .sum
+  def partOne(firstList: List[Int], secondList: List[Int]) =
+    firstList.sorted
+      .zip(secondList.sorted)
+      .map((leftNumber, rightNumber) => (rightNumber - leftNumber).abs)
+      .sum
 
-def partTwo(firstList: List[Int], secondList: List[Int]) =
-  firstList.map { a =>
-    val countInB = secondList.count(_ == a)
-    a * countInB
-  }.sum
+  def partTwo(firstList: List[Int], secondList: List[Int]) =
+    firstList.map { a =>
+      val countInB = secondList.count(_ == a)
+      a * countInB
+    }.sum
 
-def getLeftAndRightLists(lines: List[String]) =
-  lines.map { line =>
-    val pair = line.split("\\s+")
-    (pair(0).toInt, pair(1).toInt)
-  }.unzip
+  def getLeftAndRightLists(lines: List[String]) =
+    lines.map { line =>
+      val pair = line.split("\\s+")
+      (pair(0).toInt, pair(1).toInt)
+    }.unzip
+
+}
